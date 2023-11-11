e3f3557ac70a245f0719d820af7456628a821464
kudidev <144379247+kudidev@users.noreply.github.com>
2023-11-11 15:42:27 -0800

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -36,6 +36,6 @@ public class NumericApplicationTests {
     @Test
     public void welcomeMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Kubernetes DevSecOps"));
+                .andExpect(content().string("Kubernetes DevSecOps is the very best"));
     }
 }
